d4d1d2acba459e84a258cf125e39d2103c5c7ad0
akiyoshi220 <ak220@hotmail.com>
2021-06-29 11:51:56 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 小池　亮義 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
